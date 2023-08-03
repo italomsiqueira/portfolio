2f6708d0dadc38ffbe73cb68e2426aea8d08b256
Italo Siqueira <italomsiqueira@gmail.com>
2023-08-03 19:18:02 -0300

    initial commit

---
+++ assets/js/form-submit.js
@@ -0,0 +1,73 @@
+function validarEmail(){
+    var email = document.querySelector('emailEmail');
+
+    if(!email.checkValidity()){
+        document.getElementById('erroEmail').removeAttribute('hidden');
+    }
+}
+
+class FormSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(settings.form);
+        this.formButton = document.querySelector(settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+
+    displaySuccess() {
+        this.form.innerHTML = this.settings.success;
+    }
+
+    displayError() {
+        this.form.innerHTML = this.settings.error;
+    }
+
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+
+    onSubmission(event) {
+        event.preventDefault();
+        event.target.disabled = true;
+        event.target.innerText = "Enviando...";
+    }
+
+    async sendForm(event) {
+        try {
+            this.onSubmission(event);
+            await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
+            this.displaySuccess();
+        } catch (error) {
+            this.displayError();
+            throw new Error(error);
+        }
+    }
+
+    init() {
+        if (this.form) this.formButton.addEventListener("click", this.sendForm);
+        return this;
+    }
+}
+
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<div class='alert alert-success col-md-12'><strong>Mensagem enviada!</strong></div>",
+    error: "<div class='alert alert-danger col-md-12'><strong>Não foi possível enviar sua mensagem!</strong></div>",
+});
+formSubmit.init();
