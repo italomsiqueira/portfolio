65af5c12b4a8f2af8cd84aeaff0b04321b22dd95
italomsiqueira <italoms@live.com>
2025-12-04 15:19:53 -0300

    teste db

--- sigo/includes/conexao.php
+++ sigo/includes/conexao.php
@@ -4,7 +4,7 @@
 $conn = mysqli_connect(hostname: 'sql100.infinityfree.com', username: 'if0_40566558', password: 'Raquel0608');
 
 if($conn){    
-    mysqli_select_db($conn, 'sigo');
+    mysqli_select_db($conn, 'if0_40566558_sigo');
 }else{
     die('ERRO AO CONECTAR AO BD');  
 }
