6ee451e650000987d51163be1b1bbc99ec81efe6
italomsiqueira <italoms@live.com>
2025-12-04 15:37:05 -0300

    UFT8

--- sigo/includes/conexao.php
+++ sigo/includes/conexao.php
@@ -2,6 +2,7 @@
 
 //CONEXÃO COM SERVIDOR
 $conn = mysqli_connect(hostname: 'sql100.infinityfree.com', username: 'if0_40566558', password: 'Raquel0608');
+mysqli_set_charset($conn, "utf8");
 
 if($conn){    
     mysqli_select_db($conn, 'if0_40566558_sigo');
